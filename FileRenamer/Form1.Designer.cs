429213dd9053d9110f054d7dd89512d95f90d4a3
zaharenkoevgenii <zaharenko.evgenii@gmail.com>
2017-12-17 12:37:39 +0300

    Initial commit

--- FileRenamer/Form1.Designer.cs
+++ FileRenamer/Form1.Designer.cs
@@ -1,4 +1,7 @@
-﻿namespace FileRenamer
+﻿using System;
+using System.IO;
+
+namespace FileRenamer
 {
     partial class Form1
     {
@@ -28,12 +31,127 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.folderBrowser = new System.Windows.Forms.FolderBrowserDialog();
+            this.tbPath = new System.Windows.Forms.TextBox();
+            this.btBrowse = new System.Windows.Forms.Button();
+            this.rbNumeric = new System.Windows.Forms.RadioButton();
+            this.rbDate = new System.Windows.Forms.RadioButton();
+            this.rbRandom = new System.Windows.Forms.RadioButton();
+            this.rbGuid = new System.Windows.Forms.RadioButton();
+            this.btRename = new System.Windows.Forms.Button();
+            this.pbProgress = new System.Windows.Forms.ProgressBar();
+            this.SuspendLayout();
+            // 
+            // tbPath
+            // 
+            this.tbPath.Location = new System.Drawing.Point(13, 21);
+            this.tbPath.Name = "tbPath";
+            this.tbPath.ReadOnly = true;
+            this.tbPath.Size = new System.Drawing.Size(257, 20);
+            this.tbPath.TabIndex = 0;
+            this.tbPath.Text = "C:\\Users\\zahar\\Desktop";
+            // 
+            // btBrowse
+            // 
+            this.btBrowse.Location = new System.Drawing.Point(276, 19);
+            this.btBrowse.Name = "btBrowse";
+            this.btBrowse.Size = new System.Drawing.Size(75, 23);
+            this.btBrowse.TabIndex = 1;
+            this.btBrowse.Text = "Browse";
+            this.btBrowse.UseVisualStyleBackColor = true;
+            this.btBrowse.Click += new System.EventHandler(this.btBrowse_Click);
+            // 
+            // rbNumeric
+            // 
+            this.rbNumeric.AutoSize = true;
+            this.rbNumeric.Location = new System.Drawing.Point(13, 66);
+            this.rbNumeric.Name = "rbNumeric";
+            this.rbNumeric.Size = new System.Drawing.Size(62, 17);
+            this.rbNumeric.TabIndex = 2;
+            this.rbNumeric.Text = "numeric";
+            this.rbNumeric.UseVisualStyleBackColor = true;
+            // 
+            // rbDate
+            // 
+            this.rbDate.AutoSize = true;
+            this.rbDate.Location = new System.Drawing.Point(81, 66);
+            this.rbDate.Name = "rbDate";
+            this.rbDate.Size = new System.Drawing.Size(46, 17);
+            this.rbDate.TabIndex = 3;
+            this.rbDate.Text = "date";
+            this.rbDate.UseVisualStyleBackColor = true;
+            // 
+            // rbRandom
+            // 
+            this.rbRandom.AutoSize = true;
+            this.rbRandom.Checked = true;
+            this.rbRandom.Location = new System.Drawing.Point(133, 66);
+            this.rbRandom.Name = "rbRandom";
+            this.rbRandom.Size = new System.Drawing.Size(60, 17);
+            this.rbRandom.TabIndex = 4;
+            this.rbRandom.TabStop = true;
+            this.rbRandom.Text = "random";
+            this.rbRandom.UseVisualStyleBackColor = true;
+            // 
+            // rbGuid
+            // 
+            this.rbGuid.AutoSize = true;
+            this.rbGuid.Location = new System.Drawing.Point(199, 66);
+            this.rbGuid.Name = "rbGuid";
+            this.rbGuid.Size = new System.Drawing.Size(45, 17);
+            this.rbGuid.TabIndex = 5;
+            this.rbGuid.Text = "guid";
+            this.rbGuid.UseVisualStyleBackColor = true;
+            // 
+            // btRename
+            // 
+            this.btRename.Location = new System.Drawing.Point(276, 63);
+            this.btRename.Name = "btRename";
+            this.btRename.Size = new System.Drawing.Size(75, 23);
+            this.btRename.TabIndex = 6;
+            this.btRename.Text = "Rename";
+            this.btRename.UseVisualStyleBackColor = true;
+            this.btRename.Click += new System.EventHandler(this.btRename_Click);
+            // 
+            // pbProgress
+            // 
+            this.pbProgress.Location = new System.Drawing.Point(13, 21);
+            this.pbProgress.Name = "pbProgress";
+            this.pbProgress.Size = new System.Drawing.Size(257, 20);
+            this.pbProgress.TabIndex = 7;
+            this.pbProgress.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(363, 104);
+            this.Controls.Add(this.pbProgress);
+            this.Controls.Add(this.btRename);
+            this.Controls.Add(this.rbGuid);
+            this.Controls.Add(this.rbRandom);
+            this.Controls.Add(this.rbDate);
+            this.Controls.Add(this.rbNumeric);
+            this.Controls.Add(this.btBrowse);
+            this.Controls.Add(this.tbPath);
+            this.Name = "Form1";
+            this.Text = "File Renamer";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.FolderBrowserDialog folderBrowser;
+        private System.Windows.Forms.TextBox tbPath;
+        private System.Windows.Forms.Button btBrowse;
+        private System.Windows.Forms.RadioButton rbNumeric;
+        private System.Windows.Forms.RadioButton rbDate;
+        private System.Windows.Forms.RadioButton rbRandom;
+        private System.Windows.Forms.RadioButton rbGuid;
+        private System.Windows.Forms.Button btRename;
+        private System.Windows.Forms.ProgressBar pbProgress;
     }
 }
 
--- FileRenamer/Form1.cs
+++ FileRenamer/Form1.cs
@@ -1,11 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
 using System.Windows.Forms;
 
 namespace FileRenamer
@@ -15,6 +9,54 @@ namespace FileRenamer
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void btBrowse_Click(object sender, EventArgs e)
+        {
+            folderBrowser.RootFolder = Environment.SpecialFolder.DesktopDirectory;
+            if (folderBrowser.ShowDialog() == DialogResult.OK)
+            {
+                tbPath.Text = folderBrowser.SelectedPath.ToString();
+            }
+        }
+
+        private void btRename_Click(object sender, EventArgs e)
+        {
+            btBrowse.Enabled = false;
+            pbProgress.Visible = true;
+            RenameAll(Directory.GetFiles(tbPath.Text));
+            btBrowse.Enabled = true;
+            pbProgress.Visible = false;
+        }
+
+        private void RenameAll(string[] files)
+        {
+            pbProgress.Value = 0;
+            pbProgress.Maximum = files.Length;
+            for (var i = 0; i < files.Length; i++)
+            {
+                var p = Path.Combine(tbPath.Text, GetName(files, i)).ToString();
+                File.Move(files[i], p);
+                pbProgress.Value = i + 1;
+            }
+
+        }
+
+        private string GetName(string[] files,int i)
+        {
+            var ind = files[i].Length-files[i].LastIndexOf('.');
+            var ext = ind>=0?files[i].Substring(files[i].Length - ind, ind):string.Empty;
+            if (rbRandom.Checked) return GetRandom() + ext;
+            if (rbNumeric.Checked) return i.ToString()+ ext;
+            if (rbGuid.Checked) return Guid.NewGuid().ToString()+ ext;
+            if(rbDate.Checked) return File.GetCreationTime(files[i]).ToShortDateString()+"_"+i.ToString() + ext;
+            return GetRandom() + ext;
+        }
+
+        private string GetRandom()
+        {
+            var a = Path.GetRandomFileName();
+            return a.Substring(0, a.Length-4);
         }
     }
 }
